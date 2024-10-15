1a8cec7057ec9c30c62c0ea25b0aa56c2781ac50
SK Nur Ahmad <sknurahmad2@gmail.com>
2024-10-15 23:35:54 +0530

    Initial commit

---
+++ app/src/main/java/com/example/geoble/activities/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.geoble.activities
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.example.geoble.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Change App bar color
+        supportActionBar!!.setBackgroundDrawable(ColorDrawable(Color.parseColor("#9966CC")))
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = Navigation.findNavController(this, R.id.host_fragment)
+
+        NavigationUI.setupWithNavController(bottomNavigation,navController)
+    }
+}
---
+++ app/src/main/java/com/example/geoble/fragments/MyLocationFragment.kt
@@ -0,0 +1,12 @@
+package com.example.geoble.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.geoble.R
+
+class MyLocationFragment : Fragment(R.layout.fragment_my_location) {
+
+}
